11924120a5382023a2c108358eab1ba4109b0524
Mr.Steinfeger <bao66@hotmail.com>
2025-04-20 16:36:29 +0200

    first commit

---
+++ src/main/java/BaoBao/Baoshop/BaoshopApplication.java
@@ -0,0 +1,13 @@
+package BaoBao.Baoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoshopApplication.class, args);
+	}
+
+}
